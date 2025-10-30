ba527727933e3f0ba052c010ef6b5dbff20d8c68
Abizar Algifari Arham_Alprog_C <monkeywer88@gmail.com>
2025-10-30 15:21:09 +0800

    Add files via upload

---
+++ abizar program/Pertemuan 1.py
@@ -0,0 +1,8 @@
+# abizar
+print("Abizar Algifari Arham")
+print("OYY ORANG ORANG BIRAHI")
+print("PERSETAN DENGAN BIRAHI, FUCK YOU ALL")
+print("ANJING BIRAHI,LGBT,KORPORAT. FUCKYOUALL")
+# copy = alt + shift + bawah/atas
+# pindah = alt + bawah/atas
+# PERSETAN ORANG-ORANG BIRAHI, FUCK YOU ALL
