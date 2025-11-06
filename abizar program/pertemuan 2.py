d2225e6584b139884c66e2591efe40c8f56cd3e5
Abizar Algifari Arham_Alprog_C <monkeywer88@gmail.com>
2025-11-06 14:26:55 +0800

    Add files via upload

---
+++ abizar program/pertemuan 2.py
@@ -0,0 +1,43 @@
+#Variable adalah cara menyimpan nilai atau value (aljabar)
+#x: variable =: assigment 13: nilai (x=3)
+# seperti
+
+x = 13
+y = 20
+lebar = 30
+
+print("niali x adalah :", x)
+print("niali y adalah :", y)
+print("niali lebar adalah :", lebar)
+
+# ATURAN PENAMAAN VARIABLE
+# 1. tidak boleh diawali angka
+
+SKATER99 = 100
+print("nilai dari SKATER99 adalah :", SKATER99)
+
+# 2. jika lebih dari satu kata, gabung dengan_
+
+Abizar_gedir = 200
+print("niali dari Abizar_gedir :", Abizar_gedir)
+
+# 3. bisa juga menggabungkan dengan huruf kpital
+
+AbizarGedir = 300
+print("niali dari AbizarGedir :", AbizarGedir)
+
+# PEMENGGALAN INDERECT
+
+x = 123
+z = x
+print("niali dari z :",z)
+
+# tugas
+# nama saya adalah = (nama anda)
+# nim saya adalah = (nim anda)
+
+a = "Abizar Algifari Arham"
+b = 25240192
+
+print("nama saya adalah :", a)
+print("nim saya adalah :", b)
