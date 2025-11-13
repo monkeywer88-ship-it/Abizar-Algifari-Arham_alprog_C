c4f13d1f6d54296994e48e1bb06e4279905b53c1
Abizar Algifari Arham_Alprog_C <monkeywer88@gmail.com>
2025-11-13 14:32:35 +0800

    Add files via upload

---
+++ pertemuan 3.py
@@ -0,0 +1,32 @@
+# TIPE DATA
+# (Tipe dari sebuah nilai)
+
+# - Tipe data INTEGER
+x = 10
+
+# - Tipe data FLOAT
+y = 5.5
+
+# - Tipe data STRING
+Nama = "Abizar Algifari Arham"
+
+# type = mengecek data
+print("Tipe data dari x =", type(x))
+print("Tipe data dari y =", type(y))
+print("Tipe data dari Nama =", type(Nama))
+
+# - Tipe data BOOLEAN
+Kebenaran = True
+Kesalahan = False
+
+print("Tipe data dari Kebenaran =", type(Kebenaran))
+print("Tipe data dari Kesalahan =", type(Kesalahan))
+
+# Hasil dari nilai
+
+print("Tipe data dari x =", x)
+print("Tipe data dari y =", y)
+print("Tipe data dari Nama =", Nama)
+print("Tipe data dari Kebenaran =", Kebenaran)
+print("Tipe data dari Kesalahan =", Kesalahan)
+
