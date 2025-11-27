d8a395c81bad10652c32a62dc04dded8ed78c35a
Abizar Algifari Arham_Alprog_C <monkeywer88@gmail.com>
2025-11-27 15:29:12 +0800

    Add files via upload

---
+++ abizar program/pertemuan 5.py
@@ -0,0 +1,64 @@
+# Operasi Aritmatika
+# + - / x
+
+# Perioritas
+
+# 1. ()
+# 2. 2^
+# 3. x
+# 4. : 
+# 5. -
+# 6. +
+
+# Tugas
+
+# Perkalian
+# Pembagian 
+# Pengurangan
+# Pertambahan
+# Perpangkatan
+# Modulus
+# Floor Division
+
+
+# Perkalian
+a = 2
+b = 1
+nilai = a * b
+print(a, "x", b, "=", nilai)
+
+# Pembagian
+a = 4
+b = 2
+nilai = a / b
+print(a, ":", b, "=", nilai)
+
+# Pertambahan
+a = 1
+b = 1
+nilai = a + b
+print(a, "+", b, "=", nilai)
+
+# Pengurangan 
+a = 10
+b = 8
+nilai = a - b
+print(a, "-", b, "=", nilai)
+
+# Perpangkatan
+a = 2
+b = 1
+nilai = a ** b
+print(a, "^", b, "=", nilai)
+
+# Modulus
+a = 32
+b = 5
+nilai = a % b
+print(a, "%", b, "=", nilai)
+
+# Floor Division
+a = 10
+b = 4
+nilai = a // b
+print(a, "//", b, "=", nilai)
